1aac742737ad52ddb8a76d604bde9c3ff3cd19c6
ozzy ozbourne <khanosaid726@gmail.com>
2023-06-11 04:46:50 +0530

    set for 11 version

--- lib/src/main/java/io/github/ozzyozbourne/Rds.java
+++ lib/src/main/java/io/github/ozzyozbourne/Rds.java
@@ -15,25 +15,63 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *
+ */
 public final class Rds {
 
     private Rds() {
     }
 
+    /**
+     *
+     * @param pathToGet json jay way query
+     * @param pathToFile path to toml file
+     * @return The result of the jay way query
+     * @param <T> Expected java type
+     * @throws IOException when file exception occurs
+     */
     public static <T> Optional<T> srcInToml (final String pathToGet, final String pathToFile) throws IOException {
         final JsonNode jsonNode = new TomlMapper().readTree(new File(pathToFile));
         return Optional.of(JsonPath.read(jsonNode.toString(), pathToGet));
     }
 
+    /**
+     *
+     * @param pathToGet json jay way query
+     * @param pathToFile path to yaml file
+     * @return The result of the jay way query
+     * @param <T> Expected java type
+     * @throws IOException when file exception occurs
+     */
     public static <T> Optional<T> srcInYaml (final String pathToGet, final String pathToFile) throws IOException {
         final JsonNode jsonNode = new ObjectMapper().valueToTree(new Yaml().load(new FileReader(pathToFile)));
         return Optional.of(JsonPath.read(jsonNode.toString(), pathToGet));
     }
 
-    public static <T> Optional<T> srcInJsn (final String pathToFile, final String pathToGet, final Predicate... filters) throws IOException {
+    /**
+     *
+     * @param pathToGet json jay way query
+     * @param pathToFile path to json file
+     * @param filters jay json filters for query
+     * @return The result of the jay way query
+     * @param <T> Expected java type
+     * @throws IOException when file exception occurs
+     */
+    public static <T> Optional<T> srcInJsn (final String pathToGet, final String pathToFile, final Predicate... filters) throws IOException {
         return Optional.of(JsonPath.read(new FileReader(pathToFile), pathToGet, filters));
     }
 
+    /**
+     *
+     * @param filePath path to csv file
+     * @param t pojo class type
+     * @param separator csv file separator being used
+     * @param skipFirstRow whether to leave to include first row
+     * @return The result of the jay way query
+     * @param <T> Expected java type
+     * @throws IOException when file exception occurs
+     */
     public static <T> Optional<List<T>> rdCsv(final String filePath, final Class<T> t, final char separator, final boolean skipFirstRow) throws IOException {
         final CsvMapper csvMapper = CsvMapper.csvBuilder().build();
         Optional<List<T>> optionalTList;
