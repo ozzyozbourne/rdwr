8ff1abf1b1f3d226ccf0730d529e5d5e837d416d
ozzy ozbourne <khanosaid726@gmail.com>
2023-09-08 17:24:44 +0530

    bill of materails added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,4 +12,24 @@ buildCache {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("databind", "com.fasterxml.jackson.core:jackson-databind:2.15.2")
+            library("dataformatxml", "com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.2")
+            library("dataformatyaml", "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+            library("dataformattoml", "com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.15.2")
+            library("dataformatproperties", "com.fasterxml.jackson.dataformat:jackson-dataformat-properties:2.15.2")
+            library("dataformatcsv", "com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.2")
+            library("dataformatjsr", "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+            library("jsonpath", "com.jayway.jsonpath:json-path:2.8.0")
+            library("snakeyaml", "org.yaml:snakeyaml:2.1")
+            library("testng", "org.testng:testng:7.8.0")
+            library("datafaker", "net.datafaker:datafaker:1.9.0")
+            library("logbackclassic", "ch.qos.logback:logback-classic:1.4.5")
+            plugin("lombok", "io.freefair.lombok").version("8.0.1")
+        }
+    }
+}
+
 
